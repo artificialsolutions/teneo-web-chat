f0d10d16609faab541aaea188df135133b099faa
Jens Östlund <jostlund@gmail.com>
2019-02-06 10:40:35 +0100

    Make the bundle initializable

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import TIE from '@artificialsolutions/tie-api-client';
 import TeneoWebChat from './TeneoWebChat.vue';
 import MessageListCache from './utils/message-list-cache.js';
 
-const serviceName = 'teneo';
-const teneoEngineUrl = 'https://developerarea-dev.teneocloud.com/tiesdktest/';
-const teneoApi = TIE.init(teneoEngineUrl);
-const messageListCache = new MessageListCache();
+window['TeneoWebChat'] = {
+  initialize(serviceName, teneoEngineUrl) {
+    const teneoApi = TIE.init(teneoEngineUrl);
+    const messageListCache = new MessageListCache();
 
-new Vue({
-  render: (h) =>
-    h(TeneoWebChat, { props: { serviceName, teneoApi, messageListCache } }),
-}).$mount('#app');
+    new Vue({
+      render: (h) =>
+        h(TeneoWebChat, { props: { serviceName, teneoApi, messageListCache } }),
+    }).$mount('#app');
+  },
+};
