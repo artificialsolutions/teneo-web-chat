84d338103a9ec5c7a3d9d6d5672172981847d28e
Jens Östlund <jostlund@gmail.com>
2019-02-06 20:31:46 +0100

    Disable eslint error

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import TIE from '@artificialsolutions/tie-api-client';
 import TeneoWebChat from './TeneoWebChat.vue';
 import MessageListCache from './utils/message-list-cache.js';
 
+// eslint-disable-next-line
 window['TeneoWebChat'] = {
   initialize(serviceName, teneoEngineUrl) {
     const teneoApi = TIE.init(teneoEngineUrl);
