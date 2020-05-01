5486cefebb3a2d09f3831f145170832b9c1a3a72
Lucas Willering <lucas.willering@artificial-solutions.com>
2020-05-01 13:09:31 +0200

    Support for breakpoints

--- src/utils/parse-teneo-response.js
+++ src/utils/parse-teneo-response.js
@@ -1,4 +1,4 @@
-import { PARTICIPANT_BOT, TENEO_PARAM_KEY } from './constants.js';
+import { PARTICIPANT_BOT, TENEO_PARAM_KEY, TENEO_BREAKPOINTS_PARAM } from './constants.js';
 
 const defaultMessageType = 'text';
 
@@ -7,16 +7,61 @@ export default function parseTeneoResponse(teneoResponse) {
 
   const messages = [];
 
-  if (text) {
-    messages.push({
-      author: PARTICIPANT_BOT,
-      type: 'text',
-      data: { text },
-    });
+  // get breakpoints param for speech bubbles
+  // it is a list with start and end indexes that looks like this
+  // [[0, 39], [40, 67], [68, 96], [97, 97]]
+  const breakpointsparam = parameters && parameters[TENEO_BREAKPOINTS_PARAM];
+  let breakpoints;
+  try {
+    if (breakpointsparam) {
+      breakpoints = JSON.parse(breakpointsparam);
+    }
+  } catch(err) {
+    // console.log('Error: Unable to parse breakpointsparam JSON string')
   }
 
-  const messageParams = parameters && parameters[TENEO_PARAM_KEY];
+  if (breakpoints && Array.isArray(breakpoints)) {
 
+    if (text) {
+      // each breakpoint in the list is a bubble
+      for (var i = 0; i < breakpoints.length; ++i) {
+
+        try {
+          // get the start and end index for this bubble
+          var bubbleStartIndex = breakpoints[i][0];
+          var bubbleEndIndex = breakpoints[i][1];
+
+          if (!isNaN(bubbleStartIndex) && !isNaN(bubbleEndIndex)) {
+            // get the substring that needs to appear in a bubble
+            var bubbleText = text.substring(bubbleStartIndex,bubbleEndIndex).trim();
+
+            // add the bubble the list of messages, but only if it is not empty
+            if (bubbleText) {
+              messages.push({
+                author: PARTICIPANT_BOT,
+                type: 'text',
+                data: {'text': bubbleText },
+              });
+            }
+          }
+          
+        } catch (err) {
+          // console.log('Error: unexpected breakpoints value')
+        }
+      }
+    }
+  } else {
+    if (text) {
+      messages.push({
+        author: PARTICIPANT_BOT,
+        type: 'text',
+        data: { text },
+      });
+    }
+  }
+
+  // handle 'attachments'
+  const messageParams = parameters && parameters[TENEO_PARAM_KEY];
   let data;
   try {
     if (messageParams) {
@@ -25,8 +70,6 @@ export default function parseTeneoResponse(teneoResponse) {
   } catch(err) {
     // console.log('Error: Unable to parse JSON string')
   }
-
-
 
   if (data) {
     messages.push({
