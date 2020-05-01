779a25f38b80fccb5bb8f57a6121bc2e637f6195
Lucas Willering <lucas.willering@artificial-solutions.com>
2020-05-01 13:09:31 +0200

    Support for breakpoints

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
 export const PARTICIPANT_USER = 'user';
 export const PARTICIPANT_BOT = 'bot';
 export const TENEO_PARAM_KEY = 'teneowebclient';
+export const TENEO_BREAKPOINTS_PARAM = 'breakpoints';
 export const TITLE_IMAGE_URL = '';
 export const CHANNEL_PARAM = 'teneo-webchat';
