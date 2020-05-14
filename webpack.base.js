f40e3de8c75e750e53884ee0e73d0a37a3854ebd
Lucas Willering <lucas.willering@artificial-solutions.com>
2020-05-14 14:30:12 +0200

    Disable debugging

--- webpack.base.js
+++ webpack.base.js
@@ -19,7 +19,7 @@ module.exports = {
               {
                 "corejs": 3,
                 "useBuiltIns": "usage",
-                "debug": true
+                "debug": false
               }
             ]
           ],
