de2ede5159cb69cdcc8a64b21ef725a004f23bcc
Lucas Willering <lucas.willering@artificial-solutions.com>
2020-07-07 16:19:34 +0200

    Added 'call' method

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import Vue from 'vue';
 import TeneoWebChat from './TeneoWebChat.vue';
 import teneoApiPlugin from './plugins/teneo-api.js';
 import { EventBus, events } from '../src/utils/event-bus.js';
+import { API_FUNCTION_CALL_MAXIMIZE, API_FUNCTION_CALL_MINIMIZE, API_FUNCTION_CALL_SEND_INPUT, API_FUNCTION_CALL_END_SESSION, API_FUNCTION_CALL_CLEAR_HISTORY, API_FUNCTION_CALL_RESET } from '../src/utils/constants.js';
 
 // eslint-disable-next-line
 var functionMap = new Map();
@@ -28,6 +29,37 @@ window['TeneoWebChat'] = {
   },
   off(function_name){
     functionMap.delete(function_name);
+  },
+  call(function_name) {
+    switch (function_name) {
+      case API_FUNCTION_CALL_MAXIMIZE:
+        // handle function
+        
+        break
+  
+      case API_FUNCTION_CALL_MINIMIZE:
+        // handle function
+        break
+
+      case API_FUNCTION_CALL_SEND_INPUT:
+        // handle function
+        break
+
+      case API_FUNCTION_CALL_END_SESSION:
+        // handle function
+        break
+
+      case API_FUNCTION_CALL_CLEAR_HISTORY:
+        // handle function
+        break
+
+      case API_FUNCTION_CALL_RESET:
+        // handle function
+        break                      
+
+      default:
+        break
+    }
   },
   resetChat(){
     EventBus.$emit(events.RESET_SESSION);
