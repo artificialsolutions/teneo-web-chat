031d1c837dde3bc41a90d109f1b7b38bff404963
Lucas Willering <lucas.willering@artificial-solutions.com>
2020-07-14 16:09:07 +0200

    Added message type system message

---
+++ src/components/messages/SystemMessage.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="system-message" :class="messageSource">
+    <!-- eslint-disable-next-line vue/no-v-html -->
+    <p class="system-message__text" v-html="sanitizedHtmlText"></p>
+  </div>
+</template>
+
+<script>
+import sanitizeHtml from '../../utils/sanitize-html.js';
+
+export default {
+  name: 'SystemMessage',
+  props: {
+    message: {
+      type: Object,
+      required: true,
+      validator: (message) => {
+        return (
+          message &&
+          message.type === 'system' &&
+          message.data &&
+          message.data.text
+        );
+      },
+    },
+  },
+  computed: {
+    messageText() {
+      return this.message.data.text;
+    },
+    messageSource() {
+      return this.message.author;
+    },
+    sanitizedHtmlText() {
+      return sanitizeHtml(this.message.data.text);
+    },
+  },
+};
+</script>
+
+<style>
+.system-message {
+  font-weight: 400;
+  font-size: 0.9em;
+  line-height: 1.4;
+  width: 100%;
+  -webkit-font-smoothing: subpixel-antialiased;
+  color: var(--secondary-color, #6c757d);
+  text-align: center !important;
+}
+
+.system-message__text {
+  margin-top: 0.2em;
+  margin-bottom: 0.2em;
+} 
+
+.system-message a {
+  color: var(--text-link-color, #007bff);
+  text-decoration: none;
+}
+
+.system-message a:hover {
+  text-decoration: underline;
+}
+</style>
