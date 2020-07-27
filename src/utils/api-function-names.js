eeaa9dae5e95896efacabe5f7232a16a5cf70d67
Lucas Willering <lucas.willering@artificial-solutions.com>
2020-07-27 19:08:09 +0200

    Added modal message type

--- src/utils/api-function-names.js
+++ src/utils/api-function-names.js
@@ -32,3 +32,4 @@ export const API_ON_BUTTON_CLICK = "message_button_clicked";
 export const API_ON_OPEN_BUTTON_CLICK = "open_button_clicked";
 export const API_ON_CLOSE_BUTTON_CLICK = "close_button_clicked";
 export const API_ON_MINIMIZE_BUTTON_CLICK = "minimize_button_clicked";
+export const API_ON_MODAL_BUTTON_CLICK = "modal_button_clicked";
