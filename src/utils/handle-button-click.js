69ea098efdbfbaed613b8e05041d13cc87b3ab68
Lucas Willering <lucas.willering@artificial-solutions.com>
2020-08-03 17:27:48 +0200

    updated button click to new api

--- src/utils/handle-button-click.js
+++ src/utils/handle-button-click.js
@@ -2,6 +2,7 @@
 // const tmpVue = new Vue();
 import { API_ON_BUTTON_CLICK } from './api-function-names.js';
 import handleExtension from './handle-extension.js';
+import basePayload from './base-payload';
 
 export default async function handleButtonClick(button, idx, teneoApi) {
 
@@ -13,12 +14,18 @@ export default async function handleButtonClick(button, idx, teneoApi) {
 
     teneoApi.messageList = [...messages, selectedButton];
 
+    const payload = basePayload();
+    payload.button = button
     // check if there is an extension that want to intercept the new event
-    await handleExtension(API_ON_BUTTON_CLICK, button);
+    await handleExtension(API_ON_BUTTON_CLICK, payload);
+
+    if(payload.handledState.handled === true) {
+        return
+    }
 
     // only send silent input of postback exists
-    if (button.postback) {
-        await teneoApi.sendSilentMessage(button.postback);
+    if (payload.button.postback) {
+        await teneoApi.sendSilentMessage(payload.button.postback);
     }
 }
   
