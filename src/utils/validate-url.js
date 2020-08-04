0d418e2b39574e88b0bdd91c1f4d88830aeed620
Lucas Willering <lucas.willering@artificial-solutions.com>
2020-08-04 15:15:03 +0200

    Support data urls

--- src/utils/validate-url.js
+++ src/utils/validate-url.js
@@ -1,4 +1,12 @@
 export default function isValidURL(str) {
+    if (isReglarURL(str) || isDataURL(str)) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function isReglarURL(str) {
     var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
     '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
     '((\\d{1,3}\\.){3}\\d{1,3}))'+ // ip (v4) address
@@ -7,3 +15,13 @@ export default function isValidURL(str) {
     '(\\#[-a-z\\d_]*)?$','i');
     return pattern.test(str);
 }
+
+// Detecting data URLs
+// data URI - MDN https://developer.mozilla.org/en-US/docs/data_URIs
+// The "data" URL scheme: http://tools.ietf.org/html/rfc2397
+// Valid URL Characters: http://tools.ietf.org/html/rfc2396#section2
+
+function isDataURL(s) {
+    return !!s.match(isDataURL.regex);
+}
+isDataURL.regex = /^\s*data:([a-z]+\/[a-z0-9\-\+]+(;[a-z\-]+\=[a-z0-9\-]+)?)?(;base64)?,[a-z0-9\!\$\&\'\,\(\)\*\+\,\;\=\-\.\_\~\:\@\/\?\%\s]*\s*$/i;
