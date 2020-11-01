83bd8ce875f032302b97a431c7d45a19faa2d9bd
Lucas Willering <lucas.willering@artificial-solutions.com>
2020-11-01 18:22:39 +0100

    Concider iPad Pro to be a mobile device

--- src/utils/detect-mobile.js
+++ src/utils/detect-mobile.js
@@ -1,5 +1,7 @@
 export default function detectMobile(){
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(window.navigator.userAgent) ) {
+    if( (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(window.navigator.userAgent)) || 
+        // make sure iPad Pro is flagged as mobile
+        (navigator.maxTouchPoints && navigator.maxTouchPoints > 2 && /MacIntel/.test(navigator.platform))) {
         return true
     } else {
         return false
