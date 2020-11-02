0f90bae2b9d24bc560a2e8e1e7d24f18bc5b08e5
Lucas Willering <lucas.willering@artificial-solutions.com>
2020-11-02 16:11:11 +0100

    Added Swedish

--- src/utils/localized-messages.js
+++ src/utils/localized-messages.js
@@ -1,70 +1,104 @@
 export const TRANSLATED_MESSAGES = {
-    de: {
-      message: {
-        launchbutton_aria_label: 'Offnë Chat',
-        launchbutton_aria_roledescription: 'Mit einem digitalen Assistant chatten',
-        chat_window_group_aria_label: 'Chatfenster',
-        header_minimize_button_aria_label: 'Chatfenster minimisieren',
-        header_minimize_button_title: 'Chatfenster minimisieren',
-        header_close_button_aria_label: 'Chatfenster schließen',
-        header_close_button_title: 'Chatfenster schließen',
-        chat_window_spinner_aria_valuetext: 'Auf Antwort warten',
-        input_area_userinput_field_aria_label: 'Message-Eingabefeld',
-        input_area_userinput_field_placeholder: 'Bitte hier schreiben...',
-        input_area_send_button_aria_label: 'Message senden',
-        input_area_send_button_title: 'Message senden',
-        message_typing_indicator: 'Schreibt...'
-      }
-    },
-    en: {
-        message: {
-          launchbutton_aria_label: 'Open Chat',
-          launchbutton_aria_roledescription: 'Chat with a digital assistant',
-          chat_window_group_aria_label: 'Chat window',
-          header_minimize_button_aria_label: 'Minimize chat window',
-          header_minimize_button_title: 'Minimize chat window',
-          header_close_button_aria_label: 'Close chat window',
-          header_close_button_title: 'Close chat window',
-          chat_window_spinner_aria_valuetext: 'Waiting for response',
-          input_area_userinput_field_aria_label: 'Message input field',//
-          input_area_userinput_field_placeholder: 'Please type here...', //
-          input_area_send_button_aria_label: 'Send message',
-          input_area_send_button_title: 'Send message',
-          message_typing_indicator: 'Typing...'
-        }
-      },
-    es: {
-      message: {
-          launchbutton_aria_label: 'Abrir Chat',
-          launchbutton_aria_roledescription: 'Chatear con asistente virtual',
-          chat_window_group_aria_label: 'Ventana de chat',
-          header_minimize_button_aria_label: 'Minimizar ventana de chat',
-          header_minimize_button_title: 'Minimizar ventana de chat',
-          header_close_button_aria_label: 'Cerrar ventana de chat',
-          header_close_button_title: 'Cerrar ventana de chat',
-          chat_window_spinner_aria_valuetext: 'Esperando respuesta',
-          input_area_userinput_field_aria_label: 'Campo de entrada',
-          input_area_userinput_field_placeholder: 'Por favor escriba aquí...',
-          input_area_send_button_aria_label: 'Enviar mensaje',
-          input_area_send_button_title: 'Enviar mensaje',
-          message_typing_indicator: 'Escribiendo...'
-      }
-    },
-    nl: {
-      message: {
-        launchbutton_aria_label: 'Chatvenster openen',
-        launchbutton_aria_roledescription: 'Chat met een digitale assistent',
-        chat_window_group_aria_label: 'Chatvenster',
-        header_minimize_button_aria_label: 'Chatvenster minimaliseren ',
-        header_minimize_button_title: 'Chatvenster minimaliseren ',
-        header_close_button_aria_label: 'Chatvenster sluiten',
-        header_close_button_title: 'Chatvenster sluiten',
-        chat_window_spinner_aria_valuetext: 'Wacht op reactie van server',
-        input_area_userinput_field_aria_label: 'Invoerveld bericht',//
-        input_area_userinput_field_placeholder: 'Typ een bericht...', //
-        input_area_send_button_aria_label: 'Verstuur bericht',
-        input_area_send_button_title: 'Verstuur bericht',
-        message_typing_indicator: 'Aan het typen...'
-      }
+  de: {
+    message: {
+      launchbutton_aria_label: 'Offnë Chat',
+      launchbutton_aria_roledescription: 'Mit einem digitalen Assistant chatten',
+      chat_window_group_aria_label: 'Chatfenster',
+      header_minimize_button_aria_label: 'Chatfenster minimisieren',
+      header_minimize_button_title: 'Chatfenster minimisieren',
+      header_close_button_aria_label: 'Chatfenster schließen',
+      header_close_button_title: 'Chatfenster schließen',
+      chat_window_spinner_aria_valuetext: 'Auf Antwort warten',
+      input_area_userinput_field_aria_label: 'Message-Eingabefeld',
+      input_area_userinput_field_placeholder: 'Bitte hier schreiben...',
+      input_area_send_button_aria_label: 'Message senden',
+      input_area_send_button_title: 'Message senden',
+      message_typing_indicator: 'Schreibt...'
     }
-  };
+  },
+  en: {
+    message: {
+      launchbutton_aria_label: 'Open Chat',
+      launchbutton_aria_roledescription: 'Chat with a digital assistant',
+      chat_window_group_aria_label: 'Chat window',
+      header_minimize_button_aria_label: 'Minimize chat window',
+      header_minimize_button_title: 'Minimize chat window',
+      header_close_button_aria_label: 'Close chat window',
+      header_close_button_title: 'Close chat window',
+      chat_window_spinner_aria_valuetext: 'Waiting for response',
+      input_area_userinput_field_aria_label: 'Message input field',
+      input_area_userinput_field_placeholder: 'Please type here...',
+      input_area_send_button_aria_label: 'Send message',
+      input_area_send_button_title: 'Send message',
+      message_typing_indicator: 'Typing...'
+    }
+  },
+  'en-GB': {
+    message: {
+      launchbutton_aria_label: 'Open Chat',
+      launchbutton_aria_roledescription: 'Chat with a digital assistant',
+      chat_window_group_aria_label: 'Chat window',
+      header_minimize_button_aria_label: 'Minimise chat window',
+      header_minimize_button_title: 'Minimise chat window',
+      header_close_button_aria_label: 'Close chat window',
+      header_close_button_title: 'Close chat window',
+      chat_window_spinner_aria_valuetext: 'Waiting for response',
+      input_area_userinput_field_aria_label: 'Message input field',
+      input_area_userinput_field_placeholder: 'Please type here...',
+      input_area_send_button_aria_label: 'Send message',
+      input_area_send_button_title: 'Send message',
+      message_typing_indicator: 'Typing...'
+    }
+  },
+  es: {
+    message: {
+      launchbutton_aria_label: 'Abrir Chat',
+      launchbutton_aria_roledescription: 'Chatear con asistente virtual',
+      chat_window_group_aria_label: 'Ventana de chat',
+      header_minimize_button_aria_label: 'Minimizar ventana de chat',
+      header_minimize_button_title: 'Minimizar ventana de chat',
+      header_close_button_aria_label: 'Cerrar ventana de chat',
+      header_close_button_title: 'Cerrar ventana de chat',
+      chat_window_spinner_aria_valuetext: 'Esperando respuesta',
+      input_area_userinput_field_aria_label: 'Campo de entrada',
+      input_area_userinput_field_placeholder: 'Por favor escriba aquí...',
+      input_area_send_button_aria_label: 'Enviar mensaje',
+      input_area_send_button_title: 'Enviar mensaje',
+      message_typing_indicator: 'Escribiendo...'
+    }
+  },
+  nl: {
+    message: {
+      launchbutton_aria_label: 'Chatvenster openen',
+      launchbutton_aria_roledescription: 'Chat met een digitale assistent',
+      chat_window_group_aria_label: 'Chatvenster',
+      header_minimize_button_aria_label: 'Chatvenster minimaliseren',
+      header_minimize_button_title: 'Chatvenster minimaliseren',
+      header_close_button_aria_label: 'Chatvenster sluiten',
+      header_close_button_title: 'Chatvenster sluiten',
+      chat_window_spinner_aria_valuetext: 'Wacht op reactie van server',
+      input_area_userinput_field_aria_label: 'Invoerveld bericht',
+      input_area_userinput_field_placeholder: 'Typ een bericht...',
+      input_area_send_button_aria_label: 'Verstuur bericht',
+      input_area_send_button_title: 'Verstuur bericht',
+      message_typing_indicator: 'Aan het typen...'
+    }
+  },
+  sv: {
+    message: {
+      launchbutton_aria_label: 'Öppna chattfönster',
+      launchbutton_aria_roledescription: 'Chatta med en digtal assistent',
+      chat_window_group_aria_label: 'Chattfönster',
+      header_minimize_button_aria_label: 'Minimera chattfönster',
+      header_minimize_button_title: 'Minimera chattfönster',
+      header_close_button_aria_label: 'Stäng chattfönster',
+      header_close_button_title: 'Stäng chattfönster',
+      chat_window_spinner_aria_valuetext: 'Väntar på svar',
+      input_area_userinput_field_aria_label: 'Inmatningsfält för meddelande',
+      input_area_userinput_field_placeholder: 'Vänligen skriv här...',
+      input_area_send_button_aria_label: 'Skicka meddelande',
+      input_area_send_button_title: 'Skicka meddelande',
+      message_typing_indicator: 'Skriver...'
+    }
+  }
+};
