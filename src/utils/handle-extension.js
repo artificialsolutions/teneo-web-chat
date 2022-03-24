ec72df655321224b0a981685cab35c0310d0cf01
John Dave Decano <johndavedecano@gmail.com>
2022-03-25 00:45:35 +0800

    Update handle-extension.js

--- src/utils/handle-extension.js
+++ src/utils/handle-extension.js
@@ -1,16 +1,21 @@
-import Vue from 'vue';
+import Vue from "vue";
 
 const tmpVue = new Vue();
 
 export default async function handleExtension(method_name, payload) {
-    // get list of callback functions for method name
-    const extensionCallbacks = tmpVue.$extensionMethods.get(method_name);
-    // execute each callback one after another
-    if (!extensionCallbacks || extensionCallbacks.length === 0) return false;
-    var i = 0;
-    do {
-        //Get the extension function and execute it:
-        await extensionCallbacks[i](payload);
-    } while (++i !== extensionCallbacks.length);
+  const extensionCallbacks = tmpVue.$extensionMethods.get(method_name);
+  if (Array.isArray(extensionCallbacks) && extensionCallbacks.length) {
+    for (let i = 0; i < extensionCallbacks.length; ++i) {
+      try {
+        if (typeof extensionCallbacks[i] === "function") {
+          await extensionCallbacks[i](payload);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }
     return true;
+  } else {
+    return false;
+  }
 }
