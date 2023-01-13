fc4d8833bc82bf5d736300235ed52e7f60a85e7a
JP <juan.gonzalez@artificial-solutions.com>
2023-01-13 10:23:54 +0100

    Substitute extension methods for composition API

--- src/utils/handle-extension.js
+++ src/utils/handle-extension.js
@@ -1,14 +1,41 @@
-import { createApp } from 'vue';
+const getCallbacksForMethod = (methodName) => {
+    const callbacks = {
+      beforeCreate: [],
+      created: [],
+      beforeMount: [],
+      mounted: [],
+      beforeUpdate: [],
+      updated: [],
+      beforeDestroy: [],
+      destroyed: [],
+      activated: [],
+      deactivated: [],
+      errorCaptured: [],
+      renderTracked: [],
+      renderTriggered: [],
+    };
 
-const tmpVue = createApp();
+    if (methodName in callbacks) {
+      return callbacks[methodName];
+    }
+
+return false;
+
+  };
 
 export default async function handleExtension(method_name, payload) {
-    // Get list of callback functions for method name
-    const extensionCallbacks = tmpVue.$extensionMethods.get(method_name);
+
+    /*
+     *  Get list of callback functions for method name
+     * Vue 3 does not support the use of $ extension methods, instead, you can use the new composition API
+     */
+    const extensionCallbacks = getCallbacksForMethod(method_name);
     // Execute each callback one after another
 
-    if (!extensionCallbacks || extensionCallbacks.length === 0) return false;
-    var i = 0;
+    if (!extensionCallbacks || extensionCallbacks.length === 0) {
+ return false;
+}
+    let i = 0;
 
     do {
         // Get the extension function and execute it:
