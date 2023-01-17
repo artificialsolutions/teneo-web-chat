dcf3e8bbd6a4d29e9d0b08d647e095c93d3b7556
JP <juan.gonzalez@artificial-solutions.com>
2023-01-17 14:43:53 +0100

    rewrite Message and worked Attribute Coercion Behavior

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,41 +1,47 @@
 <template>
   <div class="twc-message" :class="messageSourceClass">
-    <component :is="messageType" :message="message"/>
+    <component :is="messageType" :message="message" />
   </div>
 </template>
 
 <script>
+import { defineComponent, computed } from 'vue';
 import messageComponentName from '../utils/message-component-name.js';
 
-export default {
+export default defineComponent({
   props: {
     message: {
       type: Object,
       required: true,
     },
   },
-  computed: {
-    messageType() {
-      if (messageComponentName(this.message.type) && this.$options.components[messageComponentName(this.message.type)]) {
+  setup() {
+    const messageType = computed(() => {
+      if (this && messageComponentName(this.message.type) && this.components[messageComponentName(this.message.type)]) {
         return messageComponentName(this.message.type);
       }
-    },
-    messageText() {
-      return this.message.data.text;
-    },
-    messageSourceClass() {
-      if (this.message.author === "bot") {
-        return "twc-bot"
-      } else if (this.message.author === "user") {
-        return "twc-user"
-      } else if (this.message.author === "agent") {
-        return "twc-agent"
-      } else {
-        return ""
+    });
+    const messageText = computed(() => this.message.data.text);
+    const messageSourceClass = computed(() => {
+      if (this) {
+        switch (this.message.value.author) {
+          case 'bot':
+            return 'twc-bot';
+          case 'user':
+            return 'twc-user';
+          case 'agent':
+            return 'twc-agent';
+          default:
+            return '';
+        }
       }
-    },
-  },
-};
+
+    });
+
+
+return { messageType, messageText, messageSourceClass };
+  }
+});
 </script>
 
 <style scoped>
