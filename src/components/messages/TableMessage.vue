500b8941bbe26307dcd2bb246c5378a6ae5d9522
JP <juan.gonzalez@artificial-solutions.com>
2023-11-29 12:05:53 +0100

    TWCD-23 refactor CSS

--- src/components/messages/TableMessage.vue
+++ src/components/messages/TableMessage.vue
@@ -99,92 +99,102 @@ computed: {
 </script>
 
 <style>
+  .twc-table {
+    border-spacing: 0 5px;
+    border-collapse: separate;
+    background: rgb(231, 231, 231);
+    border-radius: 6px;
+    overflow: hidden;
+    max-width: 400px;
+    width: 300px;
+    margin: 0 auto;
+    position: relative;
+  }
 
-.twc-table {
-border-spacing: 0 5px; 
-border-collapse: separate; 
-background: rgb(231, 231, 231);
-border-radius: 6px;
-overflow: hidden;
-max-width: 400px; 
-width: 300px;
-margin: 0 auto;
-position: relative;
-}
-.twc-table-border {
-border: none;
-box-shadow:0 2px 5px 0 rgba(0,0,0,0.16),0 2px 10px 0 rgba(0,0,0,0.12);
-}
-.twc-table-title {
-color: var(--table-title-text-color);
-background: var(--table-title-background-color);
-font-weight: bold;
-padding: 5px;
-font-size: larger;
-}
+  .twc-table-border {
+    border: none;
+    box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
+  }
 
-.twc-table-header-cell {
-color:black;
-background: rgb(231, 231, 231);
-padding: 5px;
-height: 35px;
-text-align: center;
-}
-.twc-table-header-cell:first-child {
-border-top-left-radius: 6px;
-}
+  .twc-table-title {
+    color: var(--table-title-text-color);
+    background: var(--table-title-background-color);
+    font-weight: bold;
+    padding: 5px;
+    font-size: larger;
+  }
 
-.twc-table-header-cell:last-child {
-border-top-right-radius: 6px;
-}
-.twc-table-body-row {
-height: 30px;
-background-color: rgba(247, 247, 247, 0.7)
-}
+  .twc-table-header-cell {
+    color: black;
+    background: rgb(231, 231, 231);
+    padding: 5px;
+    height: 35px;
+    text-align: center;
+  }
 
-.twc-table-body-cell:first-child {
-border-top-left-radius: 6px;
-border-bottom-left-radius: 6px;
-}
+  .twc-table-header-cell:first-child {
+    border-top-left-radius: 6px;
+  }
 
-.twc-table-body-cell:last-child {
-border-top-right-radius: 6px;
-border-bottom-right-radius: 6px;
-}
+  .twc-table-header-cell:last-child {
+    border-top-right-radius: 6px;
+  }
 
-.twc-table-body-row:hover {
-background-color: rgba(47, 40, 110, 0.8);
-color: white;
-}
+  .twc-table-body-row {
+    height: 30px;
+    background-color: rgba(247, 247, 247, 0.7);
+  }
 
-.twc-table-body-row:nth-child(even) .twc-table-body-cell {
-color: var(--table-body-even-text-color);
-background: var(--table-body-even-background-color);
-padding: 2px;
-}
+  .twc-table-body-cell:first-child {
+    border-top-left-radius: 6px;
+    border-bottom-left-radius: 6px;
+  }
 
-.twc-table-body-row:nth-child(odd) .twc-table-body-cell {
-color: var(--table-body-odd-text-color);
-background: var(--table-body-odd-background-color);
-padding: 2px;
-}
-.twc-table-footer-row:first-child {
-border-bottom-left-radius: 6px;
-}
+  .twc-table-body-cell:last-child {
+    border-top-right-radius: 6px;
+    border-bottom-right-radius: 6px;
+  }
 
-.twc-table-footer-row:last-child {
-border-bottom-right-radius: 6px;
-}
-.twc-table-footer-cell {
-color: var(--table-footer-text-color);
-background: rgb(231, 231, 231);
-padding: 2px;
-text-align: center;
-}
+  .twc-table-body-row:hover {
+    background-color: rgba(47, 40, 110, 0.8);
+    color: white;
+  }
+
+  .twc-table-body-row:nth-child(even) .twc-table-body-cell,
+  .twc-table-body-row:nth-child(odd) .twc-table-body-cell {
+    padding: 2px;
+    text-align: center;
+  }
+
+  .twc-table-body-row:nth-child(even) .twc-table-body-cell {
+    color: var(--table-body-even-text-color);
+    background: var(--table-body-even-background-color);
+  }
+
+  .twc-table-body-row:nth-child(odd) .twc-table-body-cell {
+    color: var(--table-body-odd-text-color);
+    background: var(--table-body-odd-background-color);
+  }
+
+  .twc-table-footer-row:first-child {
+    border-bottom-left-radius: 6px;
+  }
+
+  .twc-table-footer-row:last-child {
+    border-bottom-right-radius: 6px;
+  }
+
+  .twc-table-footer-cell {
+    color: var(--table-footer-text-color);
+    background: rgb(231, 231, 231);
+    padding: 2px;
+    text-align: center;
+  }
+
 </style>
 
 <style scoped>
-.td {
-  text-align: center;
+  .twc-table .td {
+    text-align: center;
   }
 </style>
