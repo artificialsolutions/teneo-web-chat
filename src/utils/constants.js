42c72b378bdd8fd5bff1e5c02a6b4ba9f07ca3a8
JP <117076001+JP-Artsol@users.noreply.github.com>
2023-12-18 16:52:42 +0100

    release info (#153)

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -10,7 +10,7 @@ export const SESSION_ID_STORAGE_KEY = 'teneo-web-chat-session-id';
 export const FALLBACK_LOCALE = 'en-US';
 
 // api version
-export const API_VERSION = "3.7.9";
+export const API_VERSION = "3.7.10";
 
 // state object keys
 export const API_KEY_VISIBILITY = "visibility";
