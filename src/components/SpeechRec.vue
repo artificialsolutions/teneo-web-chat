4afcaca3672bc47d8a2e2a2e06bfe4e0801fea5c
Roger Mortimer <roger.mortimer@artificial-solutions.com>
2024-04-22 12:49:18 +0100

    TWCD-43 - Linting fixes

--- src/components/SpeechRec.vue
+++ src/components/SpeechRec.vue
@@ -1,31 +1,35 @@
 <template>
   <div class="button-container">
-    <button v-if="asrActive"
-            @mousedown="startTranscription"
-            @mouseup="stopASR"
-            @touchstart.prevent="startTranscription"
-            @touchend.prevent="stopASR"
-            @mouseleave="handleMouseLeave"
-            type="button"
-            :class="asrButtonClass"> 
-            <span v-if="asrRecordingSymbol && buttonPressed ">{{ asrRecordingSymbol }}</span>
-            <span v-else-if="asrRecordSymbol && !buttonPressed && !transcribing">{{ asrRecordSymbol }}</span>
-            <AsrIcon v-else aria-hidden="true" />
+    <button
+      v-if="asrActive"
+      type="button"
+      :class="asrButtonClass"
+      @mousedown="startTranscription"
+      @mouseup="stopASR"
+      @touchstart.prevent="startTranscription"
+      @touchend.prevent="stopASR"
+      @mouseleave="handleMouseLeave"
+    >
+      <span v-if="asrRecordingSymbol && buttonPressed ">{{ asrRecordingSymbol }}</span>
+      <span v-else-if="asrRecordSymbol && !buttonPressed && !transcribing">{{ asrRecordSymbol }}</span>
+      <AsrIcon v-else aria-hidden="true" />
     </button>
-    <button v-if="ttsActive" 
-            @click="toggleTTS"     
-            type="button" 
-            :class="ttsButtonClass">
+    <button
+      v-if="ttsActive"
+      type="button"
+      :class="ttsButtonClass"
+      @click="toggleTTS"
+    >
       <span v-if="ttsSymbol && readIncomingMessages">{{ ttsSymbol }}</span>
-      <span v-else-if="ttsStopSymbol && !readIncomingMessages">{{ ttsStopSymbol }}</span> 
-      <MuteIcon v-else-if="!ttsStopSymbol && !readIncomingMessages"  aria-hidden="true"/>
-      <TtsIcon v-else  aria-hidden="true"/>
-    </button>    
+      <span v-else-if="ttsStopSymbol && !readIncomingMessages">{{ ttsStopSymbol }}</span>
+      <MuteIcon v-else-if="!ttsStopSymbol && !readIncomingMessages" aria-hidden="true" />
+      <TtsIcon v-else aria-hidden="true" />
+    </button>
   </div>
 </template>
 
 <script>
-import { EventBus, events } from '../utils/event-bus.js';
+import { EventBus } from '../utils/event-bus.js';
 import { mapState, mapGetters } from 'vuex';
 import AsrIcon from '../icons/asr.vue';
 import MuteIcon from '../icons/mute.vue';
@@ -37,9 +41,7 @@ export default {
     MuteIcon,
     TtsIcon
   },
-  props: {
-    userInputFieldId: String,
-  },
+
   data() {
     return {
       transcribing: false,
@@ -74,18 +76,22 @@ export default {
         'tts-disabled': !this.readIncomingMessages
       };
     }
-    
+
+  },
+
+  mounted() {
+    EventBus.$emit('tts-state-change', this.readIncomingMessages);
   },
 
   methods: {
     startTranscription() {
       this.buttonPressed = true;
-      
+
       // Before creating a new SpeechRecognition cleanup
       if (this.recognition) {
-        this.recognition.stop(); 
-        this.recognition = null; 
-      }      
+        this.recognition.stop();
+        this.recognition = null;
+      }
       const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
       if (typeof SpeechRecognition === "undefined") {
@@ -111,15 +117,16 @@ export default {
         this.recognition.stop();
       }
       this.transcribing = false;
-      this.$emit('transcribing', this.transcribing);      
+      this.$emit('transcribing', this.transcribing);
     },
 
     readTranscription(text) {
          const utterance = new SpeechSynthesisUtterance(text);
+
          window.speechSynthesis.speak(utterance);
          this.$forceUpdate();
     },
-    
+
     handleMouseLeave() {
       if (this.transcribing) {
         this.stopASR();
@@ -128,33 +135,29 @@ export default {
 
     toggleTTS() {
       window.speechSynthesis.cancel();
-       this.readIncomingMessages = !this.readIncomingMessages;       
-       EventBus.$emit('tts-state-change', this.readIncomingMessages); 
+       this.readIncomingMessages = !this.readIncomingMessages;
+       EventBus.$emit('tts-state-change', this.readIncomingMessages);
 
     },
 
     resultHandler(event) {
-      //Transcript as String
       const transcriptString = event.results[event.results.length - 1][0].transcript;
-      this.$emit('transcription', transcriptString);      
-      if (event.results[event.results.length - 1].isFinal) {        
+
+      this.$emit('transcription', transcriptString);
+      if (event.results[event.results.length - 1].isFinal) {
         this.$emit('transcriptionComplete', transcriptString);
       }
     },
-      
+
     endHandler() {
-      this.transcribing = false; 
+      this.transcribing = false;
       this.$emit('transcribing', this.transcribing);
     }
-
-  },
-  mounted(){
-    EventBus.$emit('tts-state-change', this.readIncomingMessages); 
   },
 };
 </script>
 
-  
+
 <style scoped>
 .button-container {
   max-height: 200px;
@@ -162,26 +165,26 @@ export default {
   margin-right: 2px;
   display: flex;
   flex-direction: row;
-  align-items: center; 
-  justify-content: center;   
+  align-items: center;
+  justify-content: center;
 }
 
 .asr-button, .tts-button {
-  flex: 0 0 auto; 
+  flex: 0 0 auto;
   display: flex;
-  flex-direction: row; 
+  flex-direction: row;
   justify-content: center;
-  align-items: center; 
+  align-items: center;
   cursor: pointer;
-  background-color: #f0f0f0; 
+  background-color: #f0f0f0;
   border: none;
   border-radius: 4px;
   padding: 10px;
-  margin: 5px 0; 
-  width: 44px; 
-  height: 44px; 
+  margin: 5px 0;
+  width: 44px;
+  height: 44px;
   outline: none;
-  color: var(--sendicon-fg-color,#263238); 
+  color: var(--sendicon-fg-color,#263238);
 }
 
 .custom-icon {
@@ -192,32 +195,31 @@ export default {
   padding-bottom: 10px;
   width: 24px;
   height: 44px;
-  cursor: pointer;  
+  cursor: pointer;
 }
 
 .custom-icon.asr-idle
 .custom-icon.tts-enabled
  {
-  color: var(--sendicon-fg-color,#263238); 
+  color: var(--sendicon-fg-color,#263238);
 }
 
 @media (max-width: 450px) {
   .asr-button, .tts-button {
     width: 44px;
-    height: 44px; 
+    height: 44px;
     padding: 15px;
     margin: 10px 0;
   }
 }
 .custom-icon.tts-disabled
  {
-  color: var(--expired-color,#263238); 
+  color: var(--expired-color,#263238);
 }
 
-.custom-icon.recording 
+.custom-icon.recording
 {
-  color: var(--recording-color,#FF0000); 
+  color: var(--recording-color,#FF0000);
 }
 </style>
 
-  
