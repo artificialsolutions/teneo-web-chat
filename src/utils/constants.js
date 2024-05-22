61f2e2d21ec9aef4e19d70555fa242110d6f30c4
Roger Mortimer <roger.mortimer@artificial-solutions.com>
2024-05-22 11:50:15 +0100

    v3.7.12 - Bumped version number in prep for release

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,18 +2,18 @@ export const PARTICIPANT_USER = 'user';
 export const PARTICIPANT_BOT = 'bot';
 export const TENEO_PARAM_KEY = 'teneowebclient';
 export const TENEO_OUTPUTTEXTSEGMENTS_PARAM = 'outputTextSegmentIndexes';
-export const TENEO_TEMPLATE_INDEX = 'twcTemplateIndex'
+export const TENEO_TEMPLATE_INDEX = 'twcTemplateIndex';
 export const DEFAULT_TITLE = 'Teneo Web Chat';
 export const CHANNEL_PARAM = 'teneo-webchat';
 export const BUBBLE_DELAY = 600;
 export const SESSION_ID_STORAGE_KEY = 'teneo-web-chat-session-id';
 export const FALLBACK_LOCALE = 'en-US';
 
-// api version
-export const API_VERSION = "3.7.11";
+// API version
+export const API_VERSION = '3.7.12';
 
-// state object keys
-export const API_KEY_VISIBILITY = "visibility";
-export const API_STATE_MINIMIZED = "minimized"
-export const API_STATE_MAXIMIZED = "maximized"
+// State object keys
+export const API_KEY_VISIBILITY = 'visibility';
+export const API_STATE_MINIMIZED = 'minimized';
+export const API_STATE_MAXIMIZED = 'maximized';
 
